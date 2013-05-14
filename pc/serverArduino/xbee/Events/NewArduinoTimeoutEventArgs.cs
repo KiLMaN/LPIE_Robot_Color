ed1a76e19ed81f3d59848fe44ee24f5e618f5e6e
Brice Gaulupeau <bricegdu95@hotmail.fr>
2013-05-14 16:15:10 +0200

    fuck2

---
+++ pc/serverArduino/xbee/Events/NewArduinoTimeoutEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xbee.Communication;
+
+namespace xbee.Communication.Events
+{
+    class NewArduinoTimeoutEventArgs : EventArgs
+    {
+        private byte _id;
+
+        public NewArduinoTimeoutEventArgs(byte id)
+		{
+            _id = id;
+		}
+
+        public byte Id
+		{
+			get {
+                return _id;
+			}
+		}
+    }
+}
